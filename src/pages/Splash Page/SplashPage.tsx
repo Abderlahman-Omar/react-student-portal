47ca4bc2adda8c7f7bb18da97f3a3c848e100cdb
Abderlahman-Omar <159623837+Abderlahman-Omar@users.noreply.github.com>
2024-10-15 09:35:31 +0300

    splash change

--- src/pages/Splash Page/SplashPage.tsx
+++ src/pages/Splash Page/SplashPage.tsx
@@ -100,13 +100,13 @@ export default function SplashPage() {
                                     {t("title2")}
                                 </p>
                             </div>
-                            {/* <div>
-                                    <img
-                                        className="md:w-96 md:h-96 w-48 h-48 md:-translate-y-0 translate-y-10"
-                                        src="src\assets\images\splash.png"
-                                        alt=""
-                                    />
-                                </div> */}
+                            <div>
+                                <img
+                                    className="md:w-96 md:h-96 w-48 h-48 md:-translate-y-0 translate-y-10"
+                                    src="src\assets\images\splash.png"
+                                    alt=""
+                                />
+                            </div>
                         </div>
                         <button
                             type="button"
@@ -134,6 +134,19 @@ export default function SplashPage() {
                                 />
                             </svg>
                         </button>
+                        <div>
+                            <p>Hello</p>
+                            <p>Hello</p>
+                            <p>Hello</p>
+                            <p>Hello</p>
+                            <p>Hello</p>
+                            <p>Hello</p>
+                            <p>Hello</p>
+                            <p>Hello</p>
+                            <p>Hello</p>
+                            <p>Hello</p>
+                            <p>Hello</p>
+                        </div>
                     </div>
                 </div>
             </div>
